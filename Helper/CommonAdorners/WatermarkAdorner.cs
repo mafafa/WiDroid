259382c25d74733429c9576179a884cda7de9f5e
Mathieu-Alexandre Fafard <canada_coke@msn.com>
2016-09-06 21:11:10 -0400

    - Added watermark behavior/adorner to Helper project

---
+++ Helper/CommonAdorners/WatermarkAdorner.cs
@@ -0,0 +1,83 @@
+﻿/*
+ * Code taken and modified from http://stackoverflow.com/questions/833943/watermark-hint-text-placeholder-textbox-in-wpf
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Media;
+
+
+namespace Helper.CommonAdorners
+{
+    public class WatermarkAdorner : Adorner
+    {
+        private String _watermarkText;
+        private TextBlock _watermarkTextBlock;
+        private ContentPresenter _contentPresenter;
+        
+        public WatermarkAdorner(UIElement adornedElement, String watermarkText)
+            : base(adornedElement)
+        {
+            WatermarkText = watermarkText;
+            _watermarkTextBlock = new TextBlock();
+
+            IsHitTestVisible = false;
+
+            // Watermark Style
+            _contentPresenter = new ContentPresenter();
+            _watermarkTextBlock.Text = WatermarkText;
+            _watermarkTextBlock.FontStyle = FontStyles.Italic;
+            _contentPresenter.Content = _watermarkTextBlock;
+            _contentPresenter.Opacity = 0.6;
+            _contentPresenter.Margin = new Thickness(5, 0, 0, 0);
+
+            // Hide the control adorner when the adorned element is hidden
+            Binding binding = new Binding("IsVisible");
+            binding.Source = AdornedElement;
+            binding.Converter = new BooleanToVisibilityConverter();
+            SetBinding(VisibilityProperty, binding);
+        }
+
+        protected override int VisualChildrenCount
+        {
+            get { return 1; }
+        }
+
+        protected override Visual GetVisualChild(int index)
+        {
+            return _contentPresenter;
+        }
+
+        protected override Size MeasureOverride(Size constraint)
+        {
+            // Here's the secret to getting the adorner to cover the whole control
+            _contentPresenter.Measure(Control.RenderSize);
+            return Control.RenderSize;
+        }
+
+        protected override Size ArrangeOverride(Size finalSize)
+        {
+            _contentPresenter.Arrange(new Rect(finalSize));
+            return finalSize;
+        }
+
+        public String WatermarkText
+        {
+            get { return _watermarkText; }
+            set { _watermarkText = value; }
+        }
+
+        public Control Control
+        {
+            get { return (Control)AdornedElement; }
+        }
+    }
+}
---
+++ Helper/CommonBehaviors/WatermarkBehavior.cs
@@ -0,0 +1,118 @@
+﻿/*
+ * Code taken and modified from http://stackoverflow.com/questions/833943/watermark-hint-text-placeholder-textbox-in-wpf
+ */
+
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Documents;
+
+using Helper.CommonAdorners;
+
+
+namespace Helper.CommonBehaviors
+{
+    public static class WatermarkBehavior
+    {
+        public static readonly DependencyProperty WatermarkTextProperty =
+            DependencyProperty.RegisterAttached(
+                    "WatermarkText",
+                    typeof(String),
+                    typeof(WatermarkBehavior),
+                    new UIPropertyMetadata(String.Empty, OnWatermarkChanged));
+
+        public static String GetWatermarkText(DependencyObject obj)
+        {
+            return (String)obj.GetValue(WatermarkTextProperty);
+        }
+
+        public static void SetWatermarkText(DependencyObject obj, String value)
+        {
+            obj.SetValue(WatermarkTextProperty, value);
+        }
+
+        private static void OnWatermarkChanged(DependencyObject obj, DependencyPropertyChangedEventArgs args)
+        {
+            Control control = (Control)obj;
+            if ((control != null) && (obj is TextBox))
+            {
+                if ((String)args.NewValue != String.Empty)
+                {
+                    control.Loaded += ControlLostKeyboardFocus;
+                    control.LostKeyboardFocus += ControlLostKeyboardFocus;
+                    control.GotKeyboardFocus += ControlGotKeyboardFocus;
+                }
+                else
+                {
+                    control.Loaded -= ControlLostKeyboardFocus;
+                    control.LostKeyboardFocus -= ControlLostKeyboardFocus;
+                    control.GotKeyboardFocus -= ControlGotKeyboardFocus;
+                }
+            }
+        }
+
+        private static void ControlLostKeyboardFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+
+            if (ShouldShowWatermark(textBox))
+            {
+                AddWatermark(textBox);
+            }
+        }
+
+        private static void ControlGotKeyboardFocus(object sender, RoutedEventArgs e)
+        {
+            TextBox textBox = (TextBox)sender;
+
+            if (ShouldShowWatermark(textBox))
+            {
+                RemoveWatermark(textBox);
+            }
+        }
+
+        private static bool ShouldShowWatermark(TextBox textBox)
+        {
+            return textBox.Text == String.Empty;
+        }
+
+        private static void AddWatermark(TextBox textBox)
+        {
+            AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(textBox);
+
+            // layer could be null if control is no longer in the visual tree
+            if (adornerLayer != null)
+            {
+                adornerLayer.Add(new WatermarkAdorner(textBox, GetWatermarkText(textBox)));
+            }
+        }
+
+        private static void RemoveWatermark(TextBox textBox)
+        {
+            AdornerLayer adornerLayer = AdornerLayer.GetAdornerLayer(textBox);
+
+            // layer could be null if control is no longer in the visual tree
+            if (adornerLayer != null)
+            {
+                Adorner[] adorners = adornerLayer.GetAdorners(textBox);
+                if (adorners == null)
+                {
+                    return;
+                }
+
+                foreach (Adorner adorner in adorners)
+                {
+                    if (adorner is WatermarkAdorner)
+                    {
+                        adorner.Visibility = Visibility.Hidden;
+                        adornerLayer.Remove(adorner);
+                    }
+                }
+            }
+        }
+    }
+}
