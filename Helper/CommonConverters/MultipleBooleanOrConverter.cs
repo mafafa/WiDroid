02f27ed7711e095bf422b3584086dbb253fda8a2
Mathieu-Alexandre Fafard <canada_coke@msn.com>
2016-09-07 18:54:31 -0400

    - Created error template for validation rule (small pop up like control that prints error message) - Validation of the setting is now done while the user writes and not on focus lost from the textbox/other control - Should probably move the validation template somewhere else since it can be reused...

---
+++ Helper/CommonConverters/MultipleBooleanOrConverter.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Data;
+
+
+namespace Helper.CommonConverters
+{
+    public class MultipleBooleanOrConverter : BaseConverter, IMultiValueConverter
+    {
+        public object Convert(object[] values, Type targetType, object parameter, CultureInfo culture)
+        {
+            foreach (object value in values)
+            {
+                if ((bool)value == true)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+
+        public object[] ConvertBack(object value, Type[] targetTypes, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
